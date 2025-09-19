b78507c01bfffb1ef62ba7fe0995fc05fbf82667
Kevin Santos <ksantosh3@miumg.edu.gt>
2025-09-18 21:31:38 -0600

    Fix test GET /hello with no name: assert status 200 and text 'hello Guest'

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -10,18 +10,20 @@ suite('Functional Tests', function () {
   this.timeout(5000);
   suite('Integration tests with chai-http', function () {
     // #1
-   suite('GET /hello with no name', function () {
-  test('responds with "hello Guest"', function (done) {
-    chai.request(server)
-      .keepOpen()       // mantiene el servidor abierto
-      .get('/hello')    // sin query string
+  suite('GET /hello with no name', function () {
+  test('Test GET /hello with no name', function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get('/hello')
       .end(function (err, res) {
-        assert.equal(res.status, 200, 'Response status should be 200'); // prueba el status
-        assert.equal(res.text, 'hello Guest', 'Response should be "hello Guest"'); // prueba el texto
-        done(); // indica que terminó la prueba asincrónica
+        assert.equal(res.status, 200, 'Response status should be 200');
+        assert.equal(res.text, 'hello Guest', 'Response should be "hello Guest"');
+        done();
       });
   });
 });
+
 
     // #2
     test('Test GET /hello with your name', function (done) {
